2ccc454855e69b4c6cba719921d8c349ee11b939
Jon Hammond <jonh1016@gmail.com>
2016-05-17 15:54:12 -0600

    Added insert methods to main.js

--- adt/main.js
+++ adt/main.js
@@ -1,8 +1,18 @@
 var Node = require('./node');
 var LinkedList = require('./linkedlist');
 
-var node1 = new Node(10);
-var node2 = new Node(20);
-var node3 = new Node(30);
+// var node1 = new Node(10);
+// var node2 = new Node(20);
+// var node3 = new Node(30);
 
-var list = new LinkedList(node1);
+var list = new LinkedList();
+
+list.insertHead(9)
+    .insertHead(8)
+    .insertTail(10)
+    .insertTail(11)
+    .insertTail(12);
+
+console.log(list.head.data); //8
+console.log(list.head.next.data); //9
+console.log(list.head.next.next.next.data); //11
